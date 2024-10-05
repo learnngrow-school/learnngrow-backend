27e731c0697de49d91a8784e0ca00ef8acb76013
TemaSaur <69428015+TemaSaur@users.noreply.github.com>
2024-10-05 18:39:18 +0500

    add tmux dev environment script

---
+++ tmux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+create_tmux_session() {
+	local session_name="$1"
+
+	tmux new-session -s "$session_name" -n "Compose" \; \
+		send-keys "docker compose up --build" C-m \; \
+		new-window -n "Run" \; \
+		send-keys "go run ." C-m \; \
+		new-window -n "Dev" \; \
+		send-keys "nvim main.go" C-m \; \
+		split-window -v \; \
+		# select-pane -t 0
+}
+
+create_tmux_session "learnngrow"
+
