99a4a95b90ef2e94d1b385d660a18064100e5240
TemaSaur <69428015+TemaSaur@users.noreply.github.com>
2024-10-18 21:54:00 +0500

    add password check util

--- auth/utils/password.go
+++ auth/utils/password.go
@@ -11,3 +11,7 @@ func Hash(password string) ([]byte, error) {
 	cost, _ := strconv.Atoi(os.Getenv("BCRYPT_COST"))
 	return bcrypt.GenerateFromPassword([]byte(password), cost)
 }
+
+func Check(given string, actual string) error {
+	return bcrypt.CompareHashAndPassword([]byte(actual), []byte(given))
+}
