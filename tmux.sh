106b0fac97f9aec9082e5b4866ef1953c793b2a6
TemaSaur <69428015+TemaSaur@users.noreply.github.com>
2024-10-21 23:38:25 +0500

    change run command to update the docs

--- tmux.sh
+++ tmux.sh
@@ -6,7 +6,7 @@ create_tmux_session() {
 	tmux new-session -s "$session_name" -n "Compose" \; \
 		send-keys "docker compose up --build" C-m \; \
 		new-window -n "Run" \; \
-		send-keys "go run ." C-m \; \
+		send-keys "swag init; echo \"\nDocs created. Starging server...\n\n\"; go run ." C-m \; \
 		new-window -n "Dev" \; \
 		send-keys "nvim main.go" C-m \; \
 		split-window -v \; \
