a7aa2771833a719fbb7219a4198a9f63757d85fa
Artemy Vakhrushev <69428015+TemaSaur@users.noreply.github.com>
2024-11-07 12:07:13 +0500

    move middleware

---
+++ internal/middlewares/jwt.go
@@ -0,0 +1,32 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	jwtUtils "learn-n-grow.dev/m/auth/utils"
+)
+
+func JWTMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var err error
+
+		token, err := c.Cookie("token")
+		if err != nil {
+			c.Set("x-jwt-err", err)
+			c.Next()
+			return
+		}
+		
+		jwtClaims, err := jwtUtils.GetData(token)
+		if err != nil {
+			c.SetCookie("token", "", -1, "/", "localhost", false, true)
+			c.Set("x-jwt-err", err)
+			c.Next()
+			return
+		}
+		email := jwtClaims["sub"].(string)
+
+		c.Set("x-email", email)
+
+		c.Next()
+	}
+}
