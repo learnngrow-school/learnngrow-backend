c3f168e1d5595949dcabe2bf442d74faa197b0fe
Artemy Vakhrushev <69428015+TemaSaur@users.noreply.github.com>
2024-11-21 17:28:41 +0500

    merge two functions
    tried replacing the util function everywhere with
    `c.AbortWithStatusJson(...)`, but turned out that it would lead to a lot
    more code and would be less convinient

--- utils/errors.go
+++ utils/errors.go
@@ -2,7 +2,6 @@ package utils
 
 import "github.com/gin-gonic/gin"
 
-func Throw(context *gin.Context, status int, err error) {
-	context.JSON(status, gin.H{"error": err.Error()})
-	context.Abort()
+func Throw(c *gin.Context, status int, err error) {
+	c.AbortWithStatusJSON(status, gin.H{"error": err.Error()})
 }
